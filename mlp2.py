4c95a17b42e0414b94f143d5d975c6949e634033
abaid <khawaja.abaid@gmail.com>
2024-07-29 11:35:18 +0500

    add batch norm

--- mlp2.py
+++ mlp2.py
@@ -2,11 +2,11 @@ import jax
 from jax import random, numpy as jnp, tree
 import flax
 from flax import linen as nn
-from flax.training.train_state import TrainState
+from flax.training import train_state
 import optax
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from typing import Sequence
+from typing import Sequence, Any
 from functools import partial
 
 
@@ -14,18 +14,23 @@ class MLP(nn.Module):
     hidden_dims: Sequence[int]
 
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x, train: bool):
         for dim in self.hidden_dims:
             x = nn.Dense(features=512)(x)
+            x = nn.BatchNorm(use_running_average=not train)(x)
             x = nn.relu(x)
         x = nn.Dense(features=10)(x)
         return x
 
 
+class TrainState(train_state.TrainState):
+    batch_stats: Any
+
+
 model = MLP((256, 128, 64))
 key1, key2 = random.split(random.key(1337), 2)
 sample_image = random.normal(key1, (1, 784))
-variables = model.init(key2, sample_image)
+variables = model.init(key2, sample_image, train=False)
 
 
 batch_size = 512
@@ -39,26 +44,31 @@ mnist_ds = mnist_ds.map(lambda x, y: (tf.reshape(x, (batch_size, -1)),
 @partial(jax.jit, static_argnums=3)
 def train_step(state: TrainState, x_batched, y_batched, epsilon=1e-15):
     def forward_and_loss(params):
-        def for_single_instance(x, y):
-            y_pred = state.apply_fn(params, x)
-            y_pred = jnp.clip(y_pred, epsilon, 1. - epsilon)
-            loss = optax.softmax_cross_entropy(y_pred, y)
-            return loss
-        return jnp.mean(jax.vmap(
-            for_single_instance, in_axes=(0, 0))(x_batched, y_batched))
-    grad_fn = jax.value_and_grad(forward_and_loss)
-    loss, grads = grad_fn(state.params)
-    new_state = state.apply_gradients(grads=grads)
+        y_pred, non_trainable_updates = state.apply_fn(
+            {'params': params, 'batch_stats': state.batch_stats}, x_batched,
+            train=True, mutable=['batch_stats'])
+        y_pred = jax.vmap(
+            jnp.clip,
+            in_axes=(0, None, None))(y_pred, epsilon, 1. - epsilon)
+        loss = optax.softmax_cross_entropy(y_pred, y_batched).mean()
+        return loss, non_trainable_updates
+    grad_fn = jax.value_and_grad(forward_and_loss, has_aux=True)
+    (loss, non_trainable_updates), grads = grad_fn(state.params)
+    state = state.apply_gradients(grads=grads)
+    state = state.replace(
+        batch_stats=non_trainable_updates['batch_stats'])
     metrics = {'loss': loss}
-    return new_state, metrics
+    return state, metrics
 
 
 lr = 0.001
 tx = optax.sgd(learning_rate=lr)
 state = TrainState.create(
     apply_fn=model.apply,
-    params=variables,
-    tx=tx)
+    params=variables['params'],
+    tx=tx,
+    batch_stats=variables['batch_stats']
+)
 
 n_epochs = 100
 n_batches = 0
